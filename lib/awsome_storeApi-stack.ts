bb6ef9b799cba98adc4df4afa3fef25aaf53cf75
Gabriel Eger <52424334+GabrielEger2@users.noreply.github.com>
2025-06-27 23:39:40 -0300

    feat: add api gateway

---
+++ lib/awsome_storeApi-stack.ts
@@ -0,0 +1,39 @@
+import * as cdk from 'aws-cdk-lib';
+import * as lambdaNodeJS from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as cwlogs from 'aws-cdk-lib/aws-logs';
+import { Construct } from 'constructs';
+
+interface AwsomeStoreApiStackProps extends cdk.StackProps {
+    productsFetchHandler: lambdaNodeJS.NodejsFunction;
+}
+
+export class AwsomeStoreApiStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: AwsomeStoreApiStackProps) {
+    super(scope, id, props);
+
+    const logGroup = new cwlogs.LogGroup(this, "AwsomeStoreApiLogGroup")
+    const api = new apigateway.RestApi(this, "AwsomeStoreApi", {
+      restApiName: "AwsomeStoreApi",
+      cloudWatchRole: true,
+      deployOptions: {
+        accessLogDestination: new apigateway.LogGroupLogDestination(logGroup),
+        accessLogFormat: apigateway.AccessLogFormat.jsonWithStandardFields({
+            httpMethod: true,
+            ip: false,
+            protocol: true,
+            requestTime: true,
+            resourcePath: true,
+            responseLength: true,
+            status: true,
+            caller: true,
+            user: false,
+        }),
+      }
+    });
+
+    const productsFetchIntegration = new apigateway.LambdaIntegration(props.productsFetchHandler)
+    const productsResource = api.root.addResource("products");
+    productsResource.addMethod("GET", productsFetchIntegration)
+  }
+}
